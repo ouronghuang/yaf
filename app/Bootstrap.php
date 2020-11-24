35e0998005b9c014990240dba5fa6babea7c00b1
欧荣凰 <ouronghuang@dingtalk.com>
2020-11-24 11:46:27 +0800

    引入 Composer

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -1,31 +1,50 @@
 <?php
+
 /**
  * @name Bootstrap
  * @author root
- * @desc 所有在Bootstrap类中, 以_init开头的方法, 都会被Yaf调用,
+ * @desc 所有在 Bootstrap 类中，以 _init 开头的方法，都会被 Yaf 调用
  * @see http://www.php.net/manual/en/class.yaf-bootstrap-abstract.php
- * 这些方法, 都接受一个参数:\Yaf\Dispatcher $dispatcher
- * 调用的次序, 和申明的次序相同
+ * 这些方法，都接受一个参数：\Yaf\Dispatcher $dispatcher
+ * 调用的次序，和申明的次序相同
  */
-class Bootstrap extends \Yaf\Bootstrap_Abstract {
 
-    public function _initConfig() {
-		//把配置保存起来
-		$arrConfig = \Yaf\Application::app()->getConfig();
-		\Yaf\Registry::set('config', $arrConfig);
-	}
+use \Yaf\{
+    Application,
+    Bootstrap_Abstract,
+    Dispatcher,
+    Loader,
+    Registry
+};
 
-	public function _initPlugin(\Yaf\Dispatcher $dispatcher) {
-		//注册一个插件
-		$objSamplePlugin = new SamplePlugin();
-		$dispatcher->registerPlugin($objSamplePlugin);
-	}
+class Bootstrap extends Bootstrap_Abstract
+{
+    public function _initComposer()
+    {
+        Loader::import(__DIR__.'/../vendor/autoload.php');
+    }
 
-	public function _initRoute(\Yaf\Dispatcher $dispatcher) {
-		//在这里注册自己的路由协议,默认使用简单路由
-	}
-	
-	public function _initView(\Yaf\Dispatcher $dispatcher) {
-		//在这里注册自己的view控制器，例如smarty,firekylin
-	}
+    public function _initConfig()
+    {
+        // 把配置保存起来
+        $arrConfig = Application::app()->getConfig();
+        Registry::set('config', $arrConfig);
+    }
+
+    public function _initPlugin(Dispatcher $dispatcher)
+    {
+        // 注册一个插件
+        // $objSamplePlugin = new SamplePlugin();
+        // $dispatcher->registerPlugin($objSamplePlugin);
+    }
+
+    public function _initRoute(Dispatcher $dispatcher)
+    {
+        // 在这里注册自己的路由协议,默认使用简单路由
+    }
+
+    public function _initView(Dispatcher $dispatcher)
+    {
+        // 在这里注册自己的 view 控制器，例如 smarty, firekylin
+    }
 }
---
+++ app/helpers.php
@@ -0,0 +1,5 @@
+<?php
+
+function dd() {
+    return 'dd';
+}
