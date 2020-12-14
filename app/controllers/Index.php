9b8863cf950e7ea99321d256578bee616ff98c54
欧荣凰 <ouronghuang@dingtalk.com>
2020-12-14 10:00:29 +0800

    加入 .htaccess

--- app/controllers/Index.php
+++ app/controllers/Index.php
@@ -14,7 +14,7 @@ class IndexController extends \Yaf\Controller_Abstract {
 		$this->getView()->assign("header", "Yaf Example");
 	}
 
-	/** 
+	/**
      * 默认动作
      * Yaf支持直接把\Yaf\Request_Abstract::getParam()得到的同名参数作为Action的形参
      * 对于如下的例子, 当访问http://yourhost/yaf_skeleton/index/index/index/name/root 的时候, 你就会发现不同
