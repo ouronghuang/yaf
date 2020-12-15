c0021b86c2160d9ba00d1930d94ed6990f3a81e6
欧荣凰 <ouronghuang@dingtalk.com>
2020-12-15 14:42:57 +0800

    修改入口文件

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
+define('YAF_START', microtime(true));
 define('APP_PATH', dirname(__FILE__, 2));
 
-$application = new \Yaf\Application(APP_PATH.'/conf/app.ini');
-
-$application->bootstrap()->run();
+$app = new \Yaf\Application(APP_PATH.'/conf/app.ini');
+$app->bootstrap()->run();
